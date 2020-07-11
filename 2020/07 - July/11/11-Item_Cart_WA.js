1104ba4cc2de8f62a805eb61e0e51bf2d76c27c5
joematune <joematune@gmail.com>
2020-07-11 23:08:01 +0200

    Add July 11th whatsapp url encoded challenge

---
+++ 2020/07 - July/11/11-Item_Cart_WA.js
@@ -0,0 +1,25 @@
+// Source - Self-made
+
+const waMenuMessage = ([ info, ...items]) => {
+    let completeOrder = '';
+    let max = 0;
+    let total = 0;
+    items.forEach(item => {
+        completeOrder += `[${item.quantity}x] ${item.name} - ${info.currency}${item.price * item.quantity}
+`
+
+        total += item.quantity * item.price;
+    });
+    completeOrder += `${''.padStart(max,'-')}
+Total: ${info.currency}${total} :)`;
+
+    return encodeURI(`https://api.whatsapp.com/send?phone=${info.phone}&text=${completeOrder}`);
+};
+
+const order1 = [
+    { phone: 0123456789, currency: 'R' },
+    { price: 120, quantity: 1, name: 'Halloumi Fries' },
+    { price: 100, quantity: 2, name: 'Falafel Rainbow Bowl' }
+];
+
+console.log(waMenuMessage(order1));
